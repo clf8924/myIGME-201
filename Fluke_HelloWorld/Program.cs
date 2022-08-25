5671fbe23bbdccda792dae3ea9ef49ce2fc23f4e
clf8924 <111778040+clf8924@users.noreply.github.com>
2022-08-24 23:20:31 -0400

    Add files via upload

---
+++ Fluke_HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluke_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prints given string; in this case, my name
+            Console.WriteLine("Collin Fluke");
+
+            // Prints a blank paragraph?
+            ObsoleteAttribute x = null; // Note: cannot use quatations to print string in place of null
+            Console.WriteLine(x);
+        }
+    }
+}
