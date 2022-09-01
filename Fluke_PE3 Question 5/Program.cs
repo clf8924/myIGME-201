dc0775defe6789619fac8fd7cd3783dcf9741519
clf8924 <111778040+clf8924@users.noreply.github.com>
2022-08-31 23:44:46 -0400

    Add files via upload

---
+++ Fluke_PE3 Question 5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluke_PE3_Question_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt for first interval
+            Console.Write("Please enter an integer: ");
+            int firstInt = Convert.ToInt32(Console.ReadLine());
+
+            // Prompt for second interval
+            Console.Write("Please enter another integer: ");
+            int secondInt = Convert.ToInt32(Console.ReadLine());
+
+            // Prompt for third interval
+            Console.Write("Please enter a third integer: ");
+            int thirdInt = Convert.ToInt32(Console.ReadLine());
+
+            // Prompt for fourth interval
+            Console.Write("Please enter the last integer: ");
+            int fourthInt = Convert.ToInt32(Console.ReadLine());
+
+            // Add and display the product of the 4 integers
+            Console.WriteLine("The product of the integers you entered is: " + (firstInt * secondInt * thirdInt * fourthInt));
+        }
+    }
+}
