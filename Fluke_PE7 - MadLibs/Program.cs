eb47098780f2431aa2c5b0dcfd477a50a5032cd7
clf8924 <111778040+clf8924@users.noreply.github.com>
2022-09-11 23:55:31 -0400

    Add files via upload
    Not finished, doesn't work

---
+++ Fluke_PE7 - MadLibs/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluke_PE7___MadLibs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Welcome and prompt for name
+            Console.WriteLine("Do you want to play MadLibs? Y or N.");
+            string yesNo = Console.ReadLine();
+            if (yesNo != "Y" || yesNo != "N")
+            {
+                Console.WriteLine("Please enter Y or N.");
+                string yesNo = Console.ReadLine();
+            } else if (yesNo == "N")
+            {
+                Console.WriteLine("Goodbye.");
+                // I don't know what the command is to exit the application and I can't find it on Google
+            } else if (yesNo == "Y") { } //Continues on
+
+            Console.WriteLine("Let's play Mad Libs! Please enter your name.");
+            string name = Console.ReadLine();
+
+            int storyCount = 0;
+
+            // Open file and read lines
+            StreamReader file;
+            file = new StreamReader("c:\\templates\\MadLibsTemplate.txt");
+            string line = null;
+            while ((line = file.ReadLine()) != null)
+            {
+                ++storyCount;
+            }
+            file.Close();
+
+            // Add a number of strings into the array for how many "stories" (lines) there are
+            string[] madLibs = new string[storyCount];
+            file = new StreamReader("c:\\templates\\MadLibsTemplate.txt");
+
+            int count = 0;
+            line = null;
+            while ((line = file.ReadLine()) != null)
+            {
+                madLibs[count] = line;\
+                madLibs[count] = madLibs[count].Replace("\\n", "\n");
+                ++count;
+            }
+            file.Close();
+
+            // Prompt for story selection
+            Console.WriteLine("Great! Choose a story from 1 to " + storyCount + ".");
+            int storyNum = Convert.ToInt32(Console.ReadLine());
+            if (storyNum > storyCount || storyNum < 0)
+            {
+                Console.WriteLine("Please enter a valid story.");
+                storyNum = int.Parse(Console.ReadLine());
+            } else
+            {
+                Console.WriteLine("You have chosen story " + storyNum + ".");
+            }
+
+            string[] words = madLibs[storyNum].Split(' ');
+
+            string[] result = null;
+
+            foreach (string word in words)
+            {
+                if ()
+                {
+                    string prompt = word.Replace("_", " ");
+                    Console.WriteLine("Enter a word that is a " + prompt + "."); // Confused about how to deliniate what's a placeholder and what isn't
+                    string userInput = Console.ReadLine();
+                    userInput += result;
+                }
+                
+            }
+
+            Console.WriteLine(madLibs[storyNum]);
+        }
+    }
+}
---
+++ Fluke_PE7 - MadLibs/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Fluke_PE7 - MadLibs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fluke_PE7 - MadLibs")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0007fcb0-7d56-4463-947b-83dc69da8f71")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
