0c1008e6282bf8de61e5c7cb9873271a02db7857
clf8924 <111778040+clf8924@users.noreply.github.com>
2022-09-25 13:58:04 -0400

    Add files via upload

---
+++ Fluke_Exam1 Q3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluke_Exam1_Q3
+{
+    internal class Program
+    {
+        delegate double MyRound(double a);  
+        static void Main(string[] args)
+        {
+            MyRound myRound;
+            myRound = new MyRound(Math.Round);
+            Console.WriteLine("Please enter a decimal to be rounded to an integer.");
+            double input = Convert.ToDouble(Console.ReadLine());
+            double result = myRound(input);
+            Console.WriteLine("Your rounded decimal is: " + result + ".");
+        }
+    }
+}
