17eaccc6a471053ba1e5f7bdaf10506fd488c895
clf8924 <111778040+clf8924@users.noreply.github.com>
2022-09-25 23:41:45 -0400

    Not Finished

---
+++ Fluke_Exam1 Q12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluke_Exam1_Q12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sName;
+            double dSalary = 30000;
+            sName = Console.ReadLine();
+            if (sName == "Collin" || sName == "collin")
+            {
+                GiveRaise(sName, ref dSalary);
+            }
+            
+        }
+        static bool GiveRaise(string name, ref double salary)
+        {
+            
+        }
+    }
+}
