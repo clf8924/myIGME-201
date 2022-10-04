54b88e75a304dbcc6d02395af363a305682f426e
clf8924 <111778040+clf8924@users.noreply.github.com>
2022-10-04 12:27:21 -0400

    AKA "Vehicles"

---
+++ Fluke_PE11 Q5/Vehicles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluke_PE11_Q5
+{
+    public abstract class Vehicle
+    {
+        public virtual void LoadPassenger() 
+        { 
+        
+        }
+    }
+    public abstract class Car : Vehicle { }
+    public interface IPassengerCarrier
+    {
+        public void LoadPassenger()
+        {
+
+        }
+    }
+    public interface IHeavyLoadCarrier { }
+    public abstract class Train : Vehicle { }
+    public class Compact : Car, IPassengerCarrier { }
+    public class SUV : Car, IPassengerCarrier { }
+    public class Pickup : Car, IPassengerCarrier, IHeavyLoadCarrier { }
+    public class PassengerTrain : Train, IPassengerCarrier { }
+    public class FreightTrain : Train, IHeavyLoadCarrier { }
+    public class _424DoubleBogey : Train, IHeavyLoadCarrier { }
+}
