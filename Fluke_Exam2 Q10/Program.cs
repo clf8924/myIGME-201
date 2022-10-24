026f19f1bedc28d8f4107f854e58b64d00854bce
clf8924 <111778040+clf8924@users.noreply.github.com>
2022-10-23 23:45:05 -0400

    Add files via upload

---
+++ Fluke_Exam2 Q10/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluke_Exam2_Q10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public abstract class Chainmail
+    {
+        public int linkCount;
+        public int weight;
+        public virtual void addLink()
+        {
+
+        }
+        public abstract void pattern();
+    }
+    public interface Weave
+    {
+        void typeOf();
+    }
+    public interface Pattern
+    {
+        void metal();
+    }
+    public class Shirt : Chainmail
+    {
+        public override void pattern()
+        {
+
+        }
+        public void metal()
+        {
+
+        }
+        public void typeOf()
+        {
+
+        }
+    }
+    public class Glove :Chainmail, Weave, Pattern
+    {
+        public override void pattern()
+        {
+
+        }
+        public void metal()
+        {
+
+        }
+        public void typeOf()
+        {
+
+        }
+    }
+}
