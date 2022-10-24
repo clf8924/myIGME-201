63a073267ed2e5680bd8f108f036098b98a0026f
clf8924 <111778040+clf8924@users.noreply.github.com>
2022-10-24 17:39:50 -0400

    Add files via upload

---
+++ Fluke_PE11 Q6/Program.cs
@@ -0,0 +1,25 @@
+﻿using Fluke_PE11_Q5;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles
+
+namespace Fluke_PE11_Q6
+{
+    internal class Program
+    {
+        public void AddPassenger(IPassengerCarrier carrier)
+        {
+            carrier.LoadPassenger()
+            Console.WriteLine(carrier.ToString());
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
